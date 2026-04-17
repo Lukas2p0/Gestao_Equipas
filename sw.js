a58d3d5cead41525331495d1f06fdc169cfeafe9
Lukas2p0 <jfaria2.0@gmail.com>
2026-04-17 17:16:21 +0100

    Upgrade cache version and enhance caching logic
    Updated cache version and improved error handling during caching.

--- sw.js
+++ sw.js
@@ -1,21 +1,30 @@
-const CACHE_NAME = 'gestor-cache-v2';
+const CACHE_NAME = 'gestor-cache-v4';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return cache.addAll(urlsToCache).catch(err => console.log('Erro ao guardar ficheiro, mas a app vai instalar:', err));
+      })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
